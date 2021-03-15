5f3f65168be2ee06c6d271498d96780d800dbe57
Sandeep Dewangan <sandeepdewangan2012@gmail.com>
2021-03-15 22:36:13 +0530

    list generate

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/pages/home_page.dart';
 import 'package:myapp/pages/login_page.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:myapp/utils/routes.dart';
 import 'package:myapp/widgets/themes.dart';
 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/models/catelog.dart';
 import 'package:myapp/widgets/drawer.dart';
+import 'package:myapp/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   final int days = 30;
   final String name = "Sandeep";
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text('Catelog App'),
       ),
-      body: Center(
-        child: Container(
-          child: Text('Welcome to $days days of flutter from $name'),
-        ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+            itemCount: dummyList.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(
+                item: dummyList[index],
+              );
+            }),
       ),
       drawer: MyDrawer(),
     );
